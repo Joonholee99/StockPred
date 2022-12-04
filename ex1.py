ccdbe1191ba59747062fec0c98f6f47eb90a4933
joonholee99 <joonholee99@gmail.com>
2022-12-04 23:47:09 +0900

    AAPL stock example

--- ex1.py
+++ ex1.py
@@ -1,1 +1,13 @@
-print("ss")
+import yfinance as yf
+
+ticker = yf.Ticker("AAPL")
+
+data = ticker.history(interval = '1h', start = '2022-12-01')
+
+print(data)
+
+
+#print(data.info["volume"])
+
+#print(data.info["averageVolume"])
+#print(data.info["averageVolume10days"])
