dbb3f06141981b500d4a43cb2e39e5a0c2693de3
joonholee99 <joonholee99@gmail.com>
2022-12-05 19:57:24 +0900

    12/5, Average Volume 만들기

--- ex1.py
+++ ex1.py
@@ -1,13 +1,26 @@
 import yfinance as yf
+import pandas as pd
+import numpy
 
-ticker = yf.Ticker("AAPL")
+ticker = yf.Ticker("AAPL")      # Stock Ticker "AAPL"
 
-data = ticker.history(interval = '1h', start = '2022-12-01')
+# History of ticker stock
+# data = ticker.history(interval = '1d', start = '2022-12-01', end = '2022-12-03')
+# print(type(data.index))
+data_1y = ticker.history(interval = '1d', period = '1y')
 
-print(data)
+data_volume = data_1y["Volume"]
+# print(data_volume[0])
 
+# Average Volume could be calculated
+i = 0
+avg_volume = []
+for i in range(0, len(data_volume)) :
+    if i < 2 :
+        i += 1
+    else:
+        avg_volume.append((data_volume[i] + data_volume[i-1] + data_volume[i-2])/3)
 
-#print(data.info["volume"])
-
-#print(data.info["averageVolume"])
-#print(data.info["averageVolume10days"])
+print(avg_volume)
+print("###########################")
+print(data_volume)
