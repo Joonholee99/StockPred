325283ea18350489e8ea021ffd16e84879b8e972
joonholee99 <joonholee99@gmail.com>
2022-12-06 19:51:32 +0900

    Index 만들기까지 완료, 어떻게 쓸지?

--- ex1.py
+++ ex1.py
@@ -1,26 +1,38 @@
 import yfinance as yf
 import pandas as pd
 import numpy
+import math
 
 ticker = yf.Ticker("AAPL")      # Stock Ticker "AAPL"
 
 # History of ticker stock
 # data = ticker.history(interval = '1d', start = '2022-12-01', end = '2022-12-03')
-# print(type(data.index))
-data_1y = ticker.history(interval = '1d', period = '1y')
+# data_1y = ticker.history(interval = '1d', period = '1y')
 
-data_volume = data_1y["Volume"]
-# print(data_volume[0])
+data_1y = ticker.history(interval = '1d', start = '2022-01-03')
+data_1y_volume = data_1y["Volume"]
 
 # Average Volume could be calculated
-i = 0
 avg_volume = []
-for i in range(0, len(data_volume)) :
+for i in range(0, len(data_1y_volume)) :
     if i < 2 :
         i += 1
     else:
-        avg_volume.append((data_volume[i] + data_volume[i-1] + data_volume[i-2])/3)
+        avg_volume.append(int((data_1y_volume[i] + data_1y_volume[i-1] + data_1y_volume[i-2])/3))
 
-print(avg_volume)
-print("###########################")
-print(data_volume)
+# Get Data Daily Volume
+data_daily_volume = ticker.history(interval = '1d', start = '2022-01-05')
+data_daily_volume = data_daily_volume["Volume"]
+
+# Get Index = Volume / Average3daysVolume
+Index = []
+for i in range(0, len(data_daily_volume)):
+    Index.append(round(data_daily_volume[i]/avg_volume[i],2))
+
+print(Index)
+
+
+# Print two volume data
+# print(avg_volume)
+# print("###########################")
+# print(data_daily_volume)
