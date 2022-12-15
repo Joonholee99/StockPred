2fc465a03cf104691e64f0a9a051e8a82912add9
joonholee99 <joonholee99@gmail.com>
2022-12-15 23:22:23 +0900

    12/15 다음날 첫시간대 데이터 추출완료

--- ex1.py
+++ ex1.py
@@ -2,37 +2,59 @@ import yfinance as yf
 import pandas as pd
 import numpy
 import math
+import matplotlib.pyplot as plt
+import datetime
+from datetime import date, timedelta
 
-ticker = yf.Ticker("AAPL")      # Stock Ticker "AAPL"
+
 
 # History of ticker stock
 # data = ticker.history(interval = '1d', start = '2022-12-01', end = '2022-12-03')
 # data_1y = ticker.history(interval = '1d', period = '1y')
 
+ticker = yf.Ticker("ABNB")      # Stock Ticker "AAPL"
 data_1y = ticker.history(interval = '1d', start = '2022-01-03')
 data_1y_volume = data_1y["Volume"]
 
-# Average Volume could be calculated
+# Average Volume calculated
 avg_volume = []
 for i in range(0, len(data_1y_volume)) :
-    if i < 2 :
+    if i < 4 :
         i += 1
     else:
-        avg_volume.append(int((data_1y_volume[i] + data_1y_volume[i-1] + data_1y_volume[i-2])/3))
+        avg_volume.append(int((data_1y_volume[i] + data_1y_volume[i-1] + data_1y_volume[i-2]+data_1y_volume[i-3]+data_1y_volume[i-4])/5))
 
 # Get Data Daily Volume
-data_daily_volume = ticker.history(interval = '1d', start = '2022-01-05')
-data_daily_volume = data_daily_volume["Volume"]
+data_daily = ticker.history(interval = '1d', start = '2022-01-07')
+data_daily_volume = data_daily["Volume"]
 
-# Get Index = Volume / Average3daysVolume
+# Get Index = Volume / Average5daysVolume
 Index = []
-for i in range(0, len(data_daily_volume)):
-    Index.append(round(data_daily_volume[i]/avg_volume[i],2))
+for i in range(0, len(data_daily_volume)-1):
+    Index.append(round((data_daily_volume[i]-avg_volume[i])/avg_volume[i]*100,2))
 
-print(Index)
+# Get daily up down
+data_daily_updown = ticker.history(interval = '1d', start = '2022-01-08')
+data_1y_updown = round((data_daily_updown["Close"] - data_daily_updown["Open"])/(data_daily_updown["Open"]) * 100, 2)
 
 
-# Print two volume data
-# print(avg_volume)
-# print("###########################")
-# print(data_daily_volume)
+
+
+########################################################################################################
+##   There is no correlation between the daily high(close) price and the volume of one day before    ####
+##   So.. Try "price after first hour" & volume                                                      ####
+######################################################################################################## 
+
+
+data_hour = ticker.history(interval  = '1h', start = '2022-01-08')
+
+data_hourly_updown = []
+for i in range(0, len(data_daily_updown)):
+    t_find = data_daily_updown.index[i]
+    t_find_str = t_find.strftime("%Y-%m-%d")
+    data_hourly_updown.append(round((data_hour[t_find_str]['Close'][0] - data_hour[t_find_str]['Open'][0])/(data_hour[t_find_str]['Open'][0]) * 100, 2))
+
+
+plt.plot(Index, data_hourly_updown,'ro')
+plt.axis([min(Index), max(Index), min(data_hourly_updown), max(data_hourly_updown)])
+plt.show()
